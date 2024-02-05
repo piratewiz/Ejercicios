3e82efa24514f5ffe04ead785840b74c886faeb8
unknown <mxkst.99@gmail.com>
2024-02-05 20:37:32 +0100

    avance ejercicios

--- ejercicio-3.js
+++ ejercicio-3.js
@@ -24,8 +24,22 @@ console.log(result3)
 
 // 1.3 Muestra mediante un console el resto de dividir 15 por 9. //
 
+console.log(15 % 9)
+
 /* 1.4 Usa el correcto operador de asignación que resultará en o = 15, 
 teniendo dos variables p = 10 y j = 5. */
 
+p = 10
+j = 5
+
+o = p + j
+console.log(o)
+
 /* 1.5 Usa el correcto operador de asignación que resultará en i = 50,
 teniendo dos variables c = 10 y m = 5. */
+
+c = 10
+m = 5
+
+i = c * m
+console.log(i)
